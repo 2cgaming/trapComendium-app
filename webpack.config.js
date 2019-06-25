8981eacd536502b7a7cdbf59cb144b1f223d78ad
Pablo Frias <pafrias90@gmail.com>
2019-06-24 17:17:56 -0700

    Include scss bundling in webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,23 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
+  devServer: {
+    port: 3000,
+    open:true,
+    inline: true,
+    compress: true,
+    contentBase: path.join(__dirname, 'dist')
+  },
   module: {
     rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
