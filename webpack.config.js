0da7b3eafc309c1b0fa1c7fffe7c08db65a5fa88
Pablo Frias <pafrias90@gmail.com>
2019-07-18 12:39:09 -0700

    Begin removing scss and implementing styled components

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,13 @@ module.exports = {
   mode: 'development',
   devServer: {
     port: 3000,
-    open:true,
+    open: true,
     inline: true,
     compress: true,
     contentBase: path.join(__dirname, 'dist')
   },
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(sass|scss)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
