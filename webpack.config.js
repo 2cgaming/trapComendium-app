f845dac62c849aeb288f6691ee07748f5a19d0ba
Pablo Frias <pafrias90@gmail.com>
2019-12-09 17:44:06 -0800

    Slight file modifications and webpack script change

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,18 @@ module.exports = {
     open: true,
     inline: true,
     compress: true,
-    contentBase: path.join(__dirname, 'dist')
+    contentBase: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
+    proxy: {
+      '/api': 'http://localhost:3001'
+    },
+    publicPath: "/trapcompendium"
   },
   entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist/trapcompendium/'),//path.resolve('../..', 'go/src/github.com/pafrias/2cgaming-api/web/trapcompendium')
+    publicPath: '/trapcompendium'
+  },
   module: {
     rules: [
       {
